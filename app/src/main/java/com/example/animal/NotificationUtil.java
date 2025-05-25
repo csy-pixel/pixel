acbb5a5ce6ddab2d2f90b4d6348c15bcd3b7e0d4
“csy-pixel” <1664801033@qq.com>
2025-05-26 00:14:05 +0800

    test commit

--- app/src/main/java/com/example/animal/NotificationUtil.java
+++ app/src/main/java/com/example/animal/NotificationUtil.java
@@ -1,7 +1,6 @@
 package com.example.animal;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
---
+++ app/src/main/java/com/example/animal/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.animal; // 请替换成你的包名
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2秒
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
